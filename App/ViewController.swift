a3be26cf07025be4ef7458050257be7e9c130b79
Natalia <nata22808@mail.ru>
2023-01-22 19:43:24 +0300

    Initial Commit

---
+++ App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Наталья Миронова on 22.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
